600fd5a9a28b29e0f020b8dad12fb7664a94f45d
Мария Андреевна <79227313534@yandex.ru>
2022-09-23 03:26:06 +0500

    4

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -30,14 +30,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthorOrReadOnly, ]
 
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+
     def get_queryset(self):
-        return get_object_or_404(Post, pk=self.kwargs.get('post_id')).comments
+        return self.get_post().comments
 
     def perform_create(self, serializer):
-        serializer.save(
-            author=self.request.user,
-            post=get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        )
+        serializer.save(author=self.request.user, post=self.get_post())
 
 
 class FollowViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
